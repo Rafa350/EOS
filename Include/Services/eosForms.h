62b5ef04a1bdb347b21c67e6cb6688a4e74fe564
Rafa350 <Rafa350@4ad52a4b-eac5-49b1-a5a4-524d969995ed>
2014-12-12 22:47:12 +0000

    git-svn-id: https://subversion.assembla.com/svn/mediasl/Projectes%20MPLAB/EOS/trunk@3815 4ad52a4b-eac5-49b1-a5a4-524d969995ed

--- Include/Services/eosForms.h
+++ Include/Services/eosForms.h
@@ -6,9 +6,13 @@
 #include "eos.h"
 #endif
 
+#include "DisplayService.h"
+
+
 #define MSG_INITIALIZE         1
 #define MSG_PAINT              2
 #define MSG_ACTIVATE           3
+#define MSG_DEACTIVATE         4
 
 #define MSG_SELECTOR_UP      100
 #define MSG_SELECTOR_DOWN    101
@@ -19,16 +23,28 @@ typedef struct __eosFormsService *eosHFormsService;
 typedef struct __eosForm *eosHForm;
 
 typedef struct {
+    eosHForm hForm;
     unsigned id;
+    union {
+        struct {
+            void *hHandle;
+            unsigned param1;
+            unsigned param2;
+        };
+        struct {
+            axHDisplayService hDisplayService;
+        } msgPaint;
+    };
 } eosFormsMessage;
 
 typedef struct {
     eosCallback onMessage;
+    unsigned privateDataSize;
 } eosFormParams;
 
 typedef struct {
+    axHDisplayService hDisplayService;
     eosCallback onMessage;
-    unsigned privateDataSize;
 } eosFormsServiceParams;
 
 
@@ -38,7 +54,12 @@ extern void eosFormsServiceTask(eosHFormsService hForms);
 extern void eosFormsSendMessage(eosHFormsService hService, eosFormsMessage *message);
 extern bool eosFormsGetMessage(eosHFormsService hService, eosFormsMessage *message);
 
-extern eosHForm eosFormsCreateForm(eosFormParams *params);
+extern eosHForm eosFormsCreateForm(eosHFormsService hService, eosFormParams *params);
+extern eosHForm eosFormsGetActiveForm(eosHFormsService hService);
+extern eosHForm eosFormsSetActiveForm(eosHFormsService hService, eosHForm hForm);
+extern void eosFormsRefresh(eosHFormsService hService, eosHForm hForm);
+
+extern void *eosFormsGetPrivateData(eosHForm hForm);
 
 
 #endif
--- Source/Services/eosForms.c
+++ Source/Services/eosForms.c
@@ -3,22 +3,24 @@
 #include "System/eosMemory.h"
 
 
-typedef enum {
-    serviceInitializing,
-    serviceRunning
+typedef enum {                         // Estats del servei
+    serviceInitializing,               // -Inicialitzant
+    serviceRunning                     // -Procesate missatges
 } ServiceStates;
 
-typedef struct __eosForm {
-    eosCallback onMessage;
-    eosHForm hNextForm;
-    void *privateData;
+typedef struct __eosForm {             // Dades del formulari
+    eosCallback onMessage;             // -Callback de cada missatge
+    eosHForm hNextForm;                // -Seguent form de la llista
+    void *privateData;                 // -Dades privades del formulari
 } eosForm;
 
-typedef struct __eosFormsService {
-    ServiceStates state;
-    eosHQueue hQueue;
-    eosForm hActiveForm;
-    eosHForm hFirstForm;
+typedef struct __eosFormsService {     // Dades del servei
+    ServiceStates state;               // -Estat del servei
+    axHDisplayService hDisplayService; // -Servei de display
+    eosCallback onMessage;             // -Calback en cada missatge
+    eosHQueue hQueue;                  // -Cua de missatges
+    eosHForm hActiveForm;              // -Formulari actiu
+    eosHForm hFirstForm;               // -Primer formulari de la llista
 } eosFormsService;
 
 
@@ -46,12 +48,14 @@ eosHFormsService eosFormsServiceInitialize(
         return NULL;
 
     hService->state = serviceInitializing;
+    hService->hDisplayService = params->hDisplayService;
+    hService->onMessage = params->onMessage;
     hService->hFirstForm = NULL;
     hService->hActiveForm = NULL;
 
     eosQueueParams queueParams;
     queueParams.itemSize = sizeof(eosFormsMessage);
-    queueParams.maxItems = 50;
+    queueParams.maxItems = eosOPTIONS_FORMS_MAX_QUEUE_SIZE;
     hService->hQueue = eosQueueCreate(&queueParams);
     
     return hService;
@@ -84,11 +88,9 @@ void eosFormsServiceTask(
             eosFormsMessage message;
 
             while (eosFormsGetMessage(hService, &message)) {
-                eosHForm hForm = hService->hActiveForm;
-                if (hForm != NULL) {
-                    if (hForm->onMessage != NULL)
-                        hForm->onMessage(hForm, &message);
-                }
+                eosHForm hForm = message.hForm;
+                if ((hForm != NULL) &&  (hForm->onMessage != NULL))
+                    hForm->onMessage(hService, &message);
             }
             break;
         }
@@ -98,13 +100,136 @@ void eosFormsServiceTask(
 
 /*************************************************************************
  *
+ *       Crea un form
  *
  *       Funcio:
+ *           eosHForm eosFormsCreateForm(
+ *               eosHFormsService hService,
+ *               eosFormParams *params)
  *
  *       Entrada:
+ *           hService: El handler del servei
+ *           params  : Parametres d'inicialitzacio
  *
  *       Retorn:
+ *           El handler del form, NULL en cas d'error
  *
+ *************************************************************************/
+
+eosHForm eosFormsCreateForm(
+    eosHFormsService hService,
+    eosFormParams *params) {
+
+    eosHForm hForm = eosAlloc(sizeof(eosForm));
+    if (hForm == NULL)
+        return NULL;
+
+    hForm->privateData = eosAlloc(params->privateDataSize);
+    hForm->onMessage = params->onMessage;
+    hForm->hNextForm = hService->hFirstForm;
+    hService->hFirstForm = hForm;
+    
+    eosFormsMessage message;
+    message.id = MSG_INITIALIZE;
+    message.hForm = hForm;
+    eosFormsSendMessage(hService, &message);
+
+    return hForm;
+}
+
+
+/*************************************************************************
+ *
+ *       Obte el formulari actiu
+ *
+ *       Funcio:
+ *           eosHForm eosFormsGetActiveForm(
+ *               eosHFormsService hService)
+ *
+ *       Entrada:
+ *           hService: El handler del servei
+ *
+ *       Retorn:
+ *           El handler del formulari actiu
+ *
+ **************************************************************************/
+
+eosHForm eosFormsGetActiveForm(
+    eosHFormsService hService) {
+
+    return hService->hActiveForm;
+}
+
+
+/*************************************************************************
+ *
+ *       Canvia el formulari actiu
+ *
+ *       Funcio:
+ *           eosHForm eosFormsSetActiveForm(
+ *               eosHFormsService hService,
+ *               eosHForm hForm)
+ *
+ *       Entrada:
+ *           hService: El handler del servei
+ *           hForm   : El formulari a activat
+ *
+ *       Retorn:
+ *           El anterior formulari actiu
+ *
+ *************************************************************************/
+
+eosHForm eosFormsSetActiveForm(
+    eosHFormsService hService,
+    eosHForm hForm) {
+    
+    eosFormsMessage message;
+
+    eosHForm hInactiveForm = hService->hActiveForm;
+    
+    message.id = MSG_DEACTIVATE;
+    message.hForm = hInactiveForm;
+    eosFormsSendMessage(hService, &message);
+    
+    hService->hActiveForm = hForm;
+
+    message.id = MSG_ACTIVATE;
+    message.hForm = hForm;
+    eosFormsSendMessage(hService, &message);
+    
+    return hInactiveForm;
+}
+
+
+void eosFormsRefresh(eosHFormsService hService, eosHForm hForm) {
+    
+    eosFormsMessage message;
+    
+    message.id = MSG_PAINT;
+    message.hForm = hForm;
+    message.msgPaint.hDisplayService = hService->hDisplayService;
+    eosFormsSendMessage(hService, &message);
+}
+
+
+void *eosFormsGetPrivateData(eosHForm hForm) {
+
+    return hForm->privateData;
+}
+
+
+/*************************************************************************
+ *
+ *       Envia un missatge a la cua
+ *
+ *       Funcio:
+ *           void eosFormsSendMessage(
+ *              eosHFormsService hService,
+ *              eosFormsMessage *message)
+ *
+ *       Entrada:
+ *           hService: El handler del servei
+ *           message : El missatge
  *
  *************************************************************************/
 
@@ -118,13 +243,22 @@ void eosFormsSendMessage(
 
 /*************************************************************************
  *
+ *       Obte un missatge de la cua
  *
  *       Funcio:
+ *           bool eosFormsGetMessage(
+ *              eosHFormsService hService,
+ *              eosFormsMessage *message)
  *
  *       Entrada:
+ *           hService: El handler del servei
+ *
+ *       Sortida:
+ *           message: El missatge retornat
  *
  *       Retorn:
- *
+ *           True si tot es correxte, false en cas d'error o no quedin
+ *           mes missatges
  *
  *************************************************************************/
 
