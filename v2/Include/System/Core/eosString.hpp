e943e33975146d0d57f7ab089e621d0cc654b0ae
Rafa350 <Rafa350@4ad52a4b-eac5-49b1-a5a4-524d969995ed>
2015-12-10 22:05:45 +0000

    git-svn-id: https://subversion.assembla.com/svn/mediasl/Projectes%20MPLAB/EOS/trunk@5421 4ad52a4b-eac5-49b1-a5a4-524d969995ed

---
+++ v2/Include/System/Core/eosString.hpp
@@ -0,0 +1,14 @@
+/* 
+ * File:   eosString.hpp
+ * Author: Rafael
+ *
+ * Created on 10 / de desembre / 2015, 23:04
+ */
+
+#ifndef EOSSTRING_HPP
+#define	EOSSTRING_HPP
+
+
+
+#endif	/* EOSSTRING_HPP */
+
