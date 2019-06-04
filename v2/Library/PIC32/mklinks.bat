c57abd762aaeb1f41175913855e8eef1aaa15f18
Rafael Serrano <rsr.private@gmail.com>
2019-06-05 00:36:43 +0200

    Restructuracio carpeta Library/STM32F4
    Signed-off-by: Rafael Serrano <rsr.private@gmail.com>

--- v2/Library/PIC32/mklinks.bat
+++ v2/Library/PIC32/mklinks.bat
@@ -1,2 +1,3 @@
 @echo off
+
 mklink /J "Harmony" "C:\Library\Microchip\Harmony\v2_06"
--- v2/Library/STM32F7/mklinks.bat
+++ v2/Library/STM32F7/mklinks.bat
@@ -1,2 +1,8 @@
 @echo off
-mklink /J "STM32Cube_FW_F7" "C:\Library\STMicroelectronics\STM32Cube\Firmwares\STM32Cube_FW_F7_V1.15.0"
+
+mklink /J "CMSIS_CORE_Include"   "C:\Library\STMicroelectronics\STM32Cube\STM32Cube_FW_F7_V1.15.0\Drivers\CMSIS\Core\Include"
+mklink /J "CMSIS_DEVICE_Include" "C:\Library\STMicroelectronics\STM32Cube\STM32Cube_FW_F7_V1.15.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include"
+mklink /J "CMSIS_DEVICE_Source"  "C:\Library\STMicroelectronics\STM32Cube\STM32Cube_FW_F7_V1.15.0\Drivers\CMSIS\Device\ST\STM32F4xx\Source"
+
+mklink /J "HAL_Include"          "C:\Library\STMicroelectronics\STM32Cube\STM32Cube_FW_F7_V1.15.0\Drivers\STM32F4xx_HAL_Driver\Inc"
+mklink /J "HAL_Source"           "C:\Library\STMicroelectronics\STM32Cube\STM32Cube_FW_F7_V1.15.0\Drivers\STM32F4xx_HAL_Driver\Src"
