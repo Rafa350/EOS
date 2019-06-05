b4f03fe4ea82370a1d70a0421ca2daafbdf42ba5
Rafael Serrano <rsr.private@gmail.com>
2019-06-05 09:31:08 +0200

    Restructuracio DisplayDemo
    Signed-off-by: Rafael Serrano <rsr.private@gmail.com>

--- v2/Library/PIC32/mklinks.bat
+++ v2/Library/PIC32/mklinks.bat
@@ -1,3 +1,4 @@
 @echo off
 
-mklink /J "Harmony" "C:\Library\Microchip\Harmony\v2_06"
+mklink /J "Harmony"        "C:\Library\Microchip\Harmony\v2_06"
+mklink /J "FreeRTOS"       "C:\Library\Microchip\Harmony\v2_06\third_party\rtos\FreeRTOS"
