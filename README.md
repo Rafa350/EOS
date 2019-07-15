71a9891f0811c3f5d841376752c22309d99aa007
Rafael Serrano <rsr.private@gmail.com>
2019-07-15 17:44:32 +0200

    Commit
    Signed-off-by: Rafael Serrano <rsr.private@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-EOS
+EOS (Embedded Operating System)
+
+Features:
+-Verified targets STM32F4xx, STM32F7xx, PIC32MX.
+-FreeRTOS scheduler.
+-Based on services.
+    Digital inputs service.
+    Digital outputs service.
+    User loop service.
+    GUI service (Graphic User Interface).
+    FSM service (Finite State Machine).
+    I2C comunication queue service.
+-Multiple examples.
+
---
+++ v2/Include/System/Graphics/eosDisplaySurface.h
@@ -0,0 +1,22 @@
+#ifndef __eosDisplaySurface__
+#define __eosDisplaySurface__
+
+
+#include "eos.h"
+#include "System/Graphics/eosColor.h"
+
+
+namespace eos {
+    
+    class IDisplaySurface {
+        public:
+            virtual void setPixel(int x, int y, const Color &color) = 0;
+            virtual void setHPixels(int x, int y int length, const Color &color) = 0;
+            virtual void setVPixels(int x, int y int length, const Color &color) = 0;
+            virtual void setPixels(int x, int y int width, int height, const Color &color) = 0;
+    }
+    
+}
+
+
+#endif // __eosDisplaySurface__
