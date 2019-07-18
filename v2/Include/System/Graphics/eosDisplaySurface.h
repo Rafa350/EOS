0ddb21a917031db239a99d6b1a76978aed1e38f2
Rafael Serrano <rsr.private@gmail.com>
2019-07-18 21:24:35 +0200

    Commit
    Signed-off-by: Rafael Serrano <rsr.private@gmail.com>

--- v2/Include/System/Graphics/eosDisplaySurface.h
+++ v2/Include/System/Graphics/eosDisplaySurface.h
@@ -7,15 +7,16 @@
 
 
 namespace eos {
-    
+
     class IDisplaySurface {
         public:
+    		virtual ~IDisplaySurface() {}
             virtual void setPixel(int x, int y, const Color &color) = 0;
             virtual void setHPixels(int x, int y int length, const Color &color) = 0;
             virtual void setVPixels(int x, int y int length, const Color &color) = 0;
             virtual void setPixels(int x, int y int width, int height, const Color &color) = 0;
     }
-    
+
 }
 
 
