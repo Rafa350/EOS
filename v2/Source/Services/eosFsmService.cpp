a866f83891d7d32ef16c5d5be6bdbdb0fda77c39
Rafael Serrano <rsr.openware@gmail.com>
2019-12-11 00:17:11 +0100

    Commit
    Signed-off-by: Rafael Serrano <rsr.openware@gmail.com>

---
+++ v2/Source/Services/eosFsmService.cpp
@@ -0,0 +1,54 @@
+#include "eos.h"
+#include "Services/eosFsmService.h"
+
+
+using namespace eos;
+
+
+/// ----------------------------------------------------------------------
+/// \brief   Constructor.
+/// \param   application: Aplicacio on afeigir el servei.
+/// \param   sm: La maquina d'estat a procesar.
+///
+FsmService::FsmService(
+    Application* application,
+    StateMachine *sm): 
+    
+    Service(application),
+    sm(sm),
+    eventQueue(10),
+    eventCallback(nullptr),
+    actionCallback(nullptr) {
+        
+}
+
+
+/// ----------------------------------------------------------------------
+/// \brief    Executa les tasques del servei.
+///
+void FsmService::onTask() {
+    
+    while (true) {
+        
+        Event event;
+        
+        if (eventQueue.get(event, 1000)) {
+            if (eventCallback != nullptr) {
+                EventArgs args;
+                eventCallback->execute(args);
+            }
+            sm->acceptEvent(event);
+        }
+    }
+}
+
+
+/// ----------------------------------------------------------------------
+/// \brief    Envia un event a la maquina d'estats pel seu procesament.
+///
+bool FsmService::acceptEvent(
+    Event event,
+    unsigned timeout) {
+    
+    return eventQueue.put(event, timeout);
+}
