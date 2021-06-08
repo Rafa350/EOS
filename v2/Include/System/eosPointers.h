91299f01a5576cf98d6447d72d9a5e6e8cdb7bc3
Rafael Serrano <rsr.openware@gmail.com>
2021-06-08 19:53:05 +0200

    Pointers
    Signed-off-by: Rafael Serrano <rsr.openware@gmail.com>

--- v2/Include/System/eosPointers.h
+++ v2/Include/System/eosPointers.h
@@ -9,11 +9,81 @@ namespace eos {
 
 	template <class T>
 	class SharedPtr {
-		private:
-			T* ptr;
-			int* ptrRefCount;
-	};
+        private:
+            T* ptr;
+            int *count;
 
+            inline void dec_count() {
+                (*count)--;
+            }
+
+            inline void inc_count() {
+                (*count)++;
+            }
+    
+        public:
+            SharedPtr(T* _ptr) {
+                ptr = _ptr;
+                count = new int(1);
+            }
+
+            SharedPtr (const SharedPtr<T>& other) {
+                ptr = other.ptr;
+                count = other.count;
+                inc_count();
+            }
+
+            ~SharedPtr() {
+                if (count != nullptr) {
+                    dec_count();
+                    if (*count == 0) {
+                        delete ptr;
+                        delete count;
+                    }
+                }
+            }
+
+            Shared Ptr<T>& operator = (const SharedPtr<T>& other) {
+                if (this != &other) {
+                    if (count != nullptr) {
+                        dec_count();
+                        if (*count == 0)
+                            delete ptr;
+                    }
+                    ptr = other.ptr;
+                    count = other.count;
+                    inc_count();
+                }
+                return *this;
+            }
+
+            int use_count() const {
+                if (count != nullptr)
+                    return *count;
+                else
+                    return 0;
+            }
+
+            void reset() {
+                if (count != nullptr) {
+                    dec_count();
+                    if(*count == 0) {
+                        delete ptr;
+                        delete count;
+                    }
+                    count = nullptr;
+                    ptr = nullptr;
+                }
+            }
+
+            inline T* operator*() {
+                return ptr;
+            }
+
+            inline T* operator ->() {
+                return ptr;
+            }
+    };
 }
 
 
