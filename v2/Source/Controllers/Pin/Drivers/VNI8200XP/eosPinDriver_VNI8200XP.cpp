6bf478522da457e14ff01266b216f7fa688e851d
Rafael Serrano <rsr.openware@gmail.com>
2024-09-19 20:14:16 +0200

    Commit
    Signed-off-by: Rafael Serrano <rsr.openware@gmail.com>

--- v2/Source/Controllers/Pin/Drivers/VNI8200XP/eosPinDriver_VNI8200XP.cpp
+++ v2/Source/Controllers/Pin/Drivers/VNI8200XP/eosPinDriver_VNI8200XP.cpp
@@ -8,6 +8,11 @@ using namespace eos;
 using namespace htl;
 
 
+/// ----------------------------------------------------------------------
+/// \brief    Construtor del objecte.
+/// \param    dev: El dispositiu
+/// \param    pinNumber: El numero del pin.
+///
 PinDriver_VNI8200XP::PinDriver_VNI8200XP(
 	Device_VNI8200XP *dev,
 	unsigned pinNumber):
@@ -20,24 +25,37 @@ PinDriver_VNI8200XP::PinDriver_VNI8200XP(
 }
 
 
+/// ----------------------------------------------------------------------
+/// \brief    Canvia l'estat de la sortida a ON.
+///
 void PinDriver_VNI8200XP::set() {
 
 	_dev->set(_pinMask);
 }
 
 
+/// ----------------------------------------------------------------------
+/// \brief    Canvia l'estat de la sortida a OFF.
+///
 void PinDriver_VNI8200XP::clear() {
 
 	_dev->clear(_pinMask);
 }
 
 
+/// ----------------------------------------------------------------------
+/// \brief    Inverteix l'estat de la sortida.
+///
 void PinDriver_VNI8200XP::toggle() {
 
 	_dev->toggle(_pinMask);
 }
 
 
+/// ----------------------------------------------------------------------
+/// \brief    Escriu l'estat de la sortida.
+/// \param    pinState: L'estat a escriure.
+///
 void PinDriver_VNI8200XP::write(
 	bool pinState) {
 
@@ -45,6 +63,9 @@ void PinDriver_VNI8200XP::write(
 }
 
 
+/// ----------------------------------------------------------------------
+/// \brief    Llegeix l'estat de la sortida.
+/// ºreturn   L'estat de la sortida ON/OFF
 bool PinDriver_VNI8200XP::read() {
 
 	return (_dev->read() & _pinMask) != 0;
